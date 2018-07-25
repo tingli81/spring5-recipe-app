ba79c46310c34a099c4076691f9d17cccf178e3f
ting <ting.li@me.com>
2018-07-25 14:48:33 -0400

    onetoone ingredients object

--- src/main/java/guru/springframework/spring5recipeapp/models/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/models/Ingredient.java
@@ -12,7 +12,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    //default is eager but showing it explicitly here
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -49,5 +51,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
